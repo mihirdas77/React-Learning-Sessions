a0ff4e86eaa4aa57413592286d64c8abe9c699c3
mihirdas77 <mihirdas7707@gmail.com>
2024-05-22 11:40:50 +0530

    added

--- React Assignments - 3/C/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer">
-      <h3>Footer</h3>
-    </div>
-  );
-}
--- React Assignments - 3/C/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="navbar">
-      <h3>Navbar</h3>
-    </div>
-  );
-}
