06bad83844bc645d1382ba689d185060527a0ec9
mihirdas77 <mihirdas7707@gmail.com>
2024-05-30 18:14:50 +0530

    added

---
+++ Final Assignment/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllRoutes from "./components/AllRoutes";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
